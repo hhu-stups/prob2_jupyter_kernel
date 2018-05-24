6ff3e0dfebc081d5641099f44b2184bd8f1599b3
dgelessus <dgelessus@users.noreply.github.com>
2018-05-24 13:08:47 +0200

    Move expression evaluation into an :eval command
    All inputs that are not commands are now passed to the :eval command
    (i. e. the current way of evaluating expression works as before).

--- src/main/java/de/prob2/jupyter/ProBKernel.java
+++ src/main/java/de/prob2/jupyter/ProBKernel.java
@@ -10,21 +10,15 @@ import java.util.regex.Pattern;
 import com.google.inject.Inject;
 import com.google.inject.Injector;
 
-import de.prob.animator.domainobjects.AbstractEvalResult;
-import de.prob.animator.domainobjects.ComputationNotCompletedResult;
-import de.prob.animator.domainobjects.EnumerationWarning;
-import de.prob.animator.domainobjects.EvalResult;
-import de.prob.animator.domainobjects.EvaluationErrorResult;
-import de.prob.animator.domainobjects.FormulaExpand;
 import de.prob.scripting.ClassicalBFactory;
 import de.prob.statespace.AnimationSelector;
 import de.prob.statespace.Trace;
-import de.prob.unicode.UnicodeTranslator;
 
 import de.prob2.jupyter.commands.BrowseCommand;
 import de.prob2.jupyter.commands.CellCommand;
 import de.prob2.jupyter.commands.CommandExecutionException;
 import de.prob2.jupyter.commands.ConstantsCommand;
+import de.prob2.jupyter.commands.EvalCommand;
 import de.prob2.jupyter.commands.ExecCommand;
 import de.prob2.jupyter.commands.GroovyCommand;
 import de.prob2.jupyter.commands.HelpCommand;
@@ -67,6 +61,7 @@ public final class ProBKernel extends BaseKernel {
 		this.lineCommands.put(":?", help);
 		this.lineCommands.put(":help", help);
 		this.lineCommands.put(":version", injector.getInstance(VersionCommand.class));
+		this.lineCommands.put(":eval", injector.getInstance(EvalCommand.class));
 		this.lineCommands.put(":load", injector.getInstance(LoadFileCommand.class));
 		this.lineCommands.put(":pref", injector.getInstance(PrefCommand.class));
 		this.lineCommands.put(":browse", injector.getInstance(BrowseCommand.class));
@@ -124,54 +119,6 @@ public final class ProBKernel extends BaseKernel {
 		}
 	}
 	
-	private @NotNull DisplayData displayDataForEvalResult(final @NotNull AbstractEvalResult aer) {
-		final StringBuilder sb = new StringBuilder();
-		final boolean error;
-		if (aer instanceof EvalResult) {
-			final EvalResult result = (EvalResult)aer;
-			sb.append(UnicodeTranslator.toUnicode(result.getValue()));
-			if (!result.getSolutions().isEmpty()) {
-				sb.append("\n\nSolution:");
-				result.getSolutions().forEach((k, v) -> {
-					sb.append("\n\t");
-					sb.append(UnicodeTranslator.toUnicode(k));
-					sb.append(" = ");
-					sb.append(UnicodeTranslator.toUnicode(v));
-				});
-			}
-			error = false;
-		} else if (aer instanceof ComputationNotCompletedResult) {
-			final ComputationNotCompletedResult result = (ComputationNotCompletedResult)aer;
-			sb.append("Computation not completed: ");
-			sb.append(result.getReason());
-			error = true;
-		} else if (aer instanceof EnumerationWarning) {
-			sb.append("UNKNOWN (FALSE with enumeration warning)");
-			error = true;
-		} else if (aer instanceof EvaluationErrorResult) {
-			final EvaluationErrorResult result = (EvaluationErrorResult)aer;
-			sb.append(result.getResult());
-			if (!result.getErrors().isEmpty()) {
-				sb.append(": ");
-				result.getErrors().forEach(s -> {
-					sb.append('\n');
-					sb.append(s);
-				});
-			}
-			error = true;
-		} else {
-			LOGGER.info("Unhandled eval result type, falling back to toString(): {}", aer.getClass());
-			sb.append(aer);
-			error = false;
-		}
-		
-		if (error) {
-			throw new UserErrorException(sb.toString());
-		} else {
-			return new DisplayData(sb.toString());
-		}
-	}
-	
 	@Override
 	public @NotNull DisplayData eval(final String expr) {
 		assert expr != null;
@@ -193,7 +140,7 @@ public final class ProBKernel extends BaseKernel {
 			return this.executeLineCommand(name, argString);
 		}
 		
-		return this.displayDataForEvalResult(this.animationSelector.getCurrentTrace().evalCurrent(expr, FormulaExpand.EXPAND));
+		return this.executeLineCommand(":eval", expr);
 	}
 	
 	@Override
