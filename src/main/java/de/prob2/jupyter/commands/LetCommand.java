d1eaeec4c1ef8f938aa59874cc8f70ec1f184dc0
dgelessus <dgelessus@users.noreply.github.com>
2018-10-15 13:22:16 +0200

    Evaluate :let expressions when variable is set, not when it it used

--- src/main/java/de/prob2/jupyter/commands/LetCommand.java
+++ src/main/java/de/prob2/jupyter/commands/LetCommand.java
@@ -5,6 +5,10 @@ import java.util.List;
 import com.google.inject.Inject;
 import com.google.inject.Injector;
 
+import de.prob.animator.domainobjects.AbstractEvalResult;
+import de.prob.animator.domainobjects.EvalResult;
+import de.prob.animator.domainobjects.FormulaExpand;
+import de.prob.statespace.AnimationSelector;
 import de.prob2.jupyter.ProBKernel;
 import de.prob2.jupyter.UserErrorException;
 
@@ -16,37 +20,44 @@ import org.jetbrains.annotations.Nullable;
 
 public final class LetCommand implements Command {
 	private final @NotNull Injector injector;
+	private final @NotNull AnimationSelector animationSelector;
 	
 	@Inject
-	public LetCommand(final @NotNull Injector injector) {
+	public LetCommand(final @NotNull Injector injector, final @NotNull AnimationSelector animationSelector) {
 		super();
 		
 		this.injector = injector;
+		this.animationSelector = animationSelector;
 	}
 	
 	@Override
 	public @NotNull String getSyntax() {
-		return ":let NAME VALUE";
+		return ":let NAME EXPR";
 	}
 	
 	@Override
 	public @NotNull String getShortHelp() {
-		return "Set the value of a local variable.";
+		return "Evaluate an expression and store it in a local variable.";
 	}
 	
 	@Override
 	public @NotNull String getHelpBody() {
-		return "Variables are available in all states and are not affected by machine loads. A variable created by `:let` shadows any identifier from the machine with the same name.\n\n**Note:** Local variables are currently stored in text form. Values must have a syntactically valid text representation, and large values may cause performance issues.";
+		return "The expression is evaluated only once, in the current state, and its value is stored. Once set, variables are available in all states and are not affected by machine loads. A variable created by `:let` shadows any identifier from the machine with the same name.\n\n**Note:** The values of local variables are currently stored in text form. Values must have a syntactically valid text representation, and large values may cause performance issues.";
 	}
 	
 	@Override
-	public @Nullable DisplayData run(final @NotNull String argString) {
+	public @NotNull DisplayData run(final @NotNull String argString) {
 		final List<String> split = CommandUtils.splitArgs(argString, 2);
 		if (split.size() != 2) {
 			throw new UserErrorException("Expected 2 arguments, not " + split.size());
 		}
-		this.injector.getInstance(ProBKernel.class).getVariables().put(split.get(0), split.get(1));
-		return null;
+		final String name = split.get(0);
+		final String expr = split.get(1);
+		final AbstractEvalResult evaluated = CommandUtils.withSourceCode(expr, () -> this.animationSelector.getCurrentTrace().evalCurrent(expr, FormulaExpand.EXPAND));
+		if (evaluated instanceof EvalResult) {
+			this.injector.getInstance(ProBKernel.class).getVariables().put(name, evaluated.toString());
+		}
+		return CommandUtils.displayDataForEvalResult(evaluated);
 	}
 	
 	@Override
