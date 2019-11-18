18a22870adc5885bc680673b8ac50b9ee473ad44
dgelessus <dgelessus@users.noreply.github.com>
2019-11-18 16:07:59 +0100

    Change :check command to not use GetMachineStructureCommand
    The ProB 2 API's structure tree is used instead.

--- src/main/java/de/prob2/jupyter/commands/CheckCommand.java
+++ src/main/java/de/prob2/jupyter/commands/CheckCommand.java
@@ -1,23 +1,25 @@
 package de.prob2.jupyter.commands;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.StringJoiner;
 import java.util.stream.Collectors;
 
 import com.google.inject.Inject;
 
-import de.prob.animator.command.GetMachineStructureCommand;
 import de.prob.animator.domainobjects.AbstractEvalResult;
-import de.prob.animator.domainobjects.FormulaExpand;
-import de.prob.animator.prologast.ASTCategory;
-import de.prob.animator.prologast.ASTFormula;
+import de.prob.animator.domainobjects.IEvalElement;
+import de.prob.model.classicalb.Assertion;
+import de.prob.model.representation.AbstractFormulaElement;
+import de.prob.model.representation.AbstractTheoremElement;
+import de.prob.model.representation.Axiom;
+import de.prob.model.representation.Invariant;
 import de.prob.statespace.AnimationSelector;
 import de.prob.statespace.Trace;
 import de.prob.unicode.UnicodeTranslator;
-
 import de.prob2.jupyter.UserErrorException;
 
 import io.github.spencerpark.jupyter.kernel.ReplacementOptions;
@@ -27,13 +29,13 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public final class CheckCommand implements Command {
-	private static final @NotNull Map<@NotNull String, @NotNull String> SECTION_NAME_MAP;
+	private static final @NotNull Map<@NotNull String, @NotNull Class<? extends AbstractTheoremElement>> CHILDREN_BASE_CLASS_MAP;
 	static {
-		final Map<String, String> sectionNameMap = new HashMap<>();
-		sectionNameMap.put("properties", "PROPERTIES");
-		sectionNameMap.put("invariant", "INVARIANTS");
-		sectionNameMap.put("assertions", "ASSERTIONS");
-		SECTION_NAME_MAP = Collections.unmodifiableMap(sectionNameMap);
+		final Map<String, Class<? extends AbstractTheoremElement>> childrenBaseClassMap = new HashMap<>();
+		childrenBaseClassMap.put("properties", Axiom.class);
+		childrenBaseClassMap.put("invariant", Invariant.class);
+		childrenBaseClassMap.put("assertions", Assertion.class);
+		CHILDREN_BASE_CLASS_MAP = Collections.unmodifiableMap(childrenBaseClassMap);
 	}
 	
 	private final @NotNull AnimationSelector animationSelector;
@@ -62,38 +64,34 @@ public final class CheckCommand implements Command {
 	
 	@Override
 	public @NotNull DisplayData run(final @NotNull String argString) {
-		if (!SECTION_NAME_MAP.containsKey(argString)) {
+		if (!CHILDREN_BASE_CLASS_MAP.containsKey(argString)) {
 			throw new UserErrorException("Don't know how to check " + argString);
 		}
-		final String categoryName = SECTION_NAME_MAP.get(argString);
+		final Class<? extends AbstractTheoremElement> childrenBaseClass = CHILDREN_BASE_CLASS_MAP.get(argString);
 		final Trace trace = this.animationSelector.getCurrentTrace();
-		final GetMachineStructureCommand cmd = new GetMachineStructureCommand();
-		trace.getStateSpace().execute(cmd);
-		final Optional<ASTCategory> category = cmd.getPrologASTList().stream()
-			.filter(ASTCategory.class::isInstance)
-			.map(ASTCategory.class::cast)
-			.filter(c -> categoryName.equals(c.getName()))
-			.findAny();
+		// Find all children of a subclass of childrenBaseClass.
+		// This needs to be done manually, because getChildrenOfType only returns children whose class *exactly* matches the given class.
+		// For example, getChildrenOfType(Invariant.class) doesn't return children with class ClassicalBInvariant.
+		final List<IEvalElement> formulas = new ArrayList<>();
+		trace.getStateSpace().getMainComponent().getChildren().forEach((clazz, children) -> {
+			if (childrenBaseClass.isAssignableFrom(clazz)) {
+				children.stream()
+					.map(childrenBaseClass::cast)
+					.map(AbstractFormulaElement::getFormula)
+					.collect(Collectors.toCollection(() -> formulas));
+			}
+		});
 		
-		if (category.isPresent()) {
-			final StringJoiner sjPlain = new StringJoiner("\n");
-			final StringJoiner sjMarkdown = new StringJoiner("\n", "|Predicate|Value|\n|---|---|\n", "");
-			category.get()
-				.getSubnodes()
-				.stream()
-				.filter(ASTFormula.class::isInstance)
-				.map(ASTFormula.class::cast)
-				.forEach(f -> {
-					final AbstractEvalResult result = trace.evalCurrent(f.getFormula(FormulaExpand.TRUNCATE));
-					sjPlain.add(f.getPrettyPrint() + " = " + CommandUtils.inlinePlainTextForEvalResult(result));
-					sjMarkdown.add("|$" + UnicodeTranslator.toLatex(f.getPrettyPrint()) + "$|" + CommandUtils.inlineMarkdownForEvalResult(result) + '|');
-				});
-			final DisplayData result = new DisplayData(sjPlain.toString());
-			result.putMarkdown(sjMarkdown.toString());
-			return result;
-		} else {
-			return new DisplayData("Machine has no " + argString);
+		final StringJoiner sjPlain = new StringJoiner("\n");
+		final StringJoiner sjMarkdown = new StringJoiner("\n", "|Predicate|Value|\n|---|---|\n", "");
+		for (final IEvalElement f : formulas) {
+			final AbstractEvalResult result = trace.evalCurrent(f);
+			sjPlain.add(f.getCode() + " = " + CommandUtils.inlinePlainTextForEvalResult(result));
+			sjMarkdown.add("|$" + UnicodeTranslator.toLatex(f.getCode()) + "$|" + CommandUtils.inlineMarkdownForEvalResult(result) + '|');
 		}
+		final DisplayData result = new DisplayData(sjPlain.toString());
+		result.putMarkdown(sjMarkdown.toString());
+		return result;
 	}
 	
 	@Override
@@ -104,7 +102,7 @@ public final class CheckCommand implements Command {
 	@Override
 	public @NotNull ReplacementOptions complete(final @NotNull String argString, final int at) {
 		final String prefix = argString.substring(0, at);
-		return new ReplacementOptions(SECTION_NAME_MAP.keySet()
+		return new ReplacementOptions(CHILDREN_BASE_CLASS_MAP.keySet()
 			.stream()
 			.filter(s -> s.startsWith(prefix))
 			.collect(Collectors.toList()), 0, argString.length());
