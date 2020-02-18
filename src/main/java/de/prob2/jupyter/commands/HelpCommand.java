cb837dfffd47af96c443b1b968318c1ec94e1de5
dgelessus <dgelessus@users.noreply.github.com>
2020-02-18 13:52:17 +0100

    Structure :help command list into categories
    This should be more readable than the previous long, alphabetical list
    of all possible commands.

--- src/main/java/de/prob2/jupyter/commands/HelpCommand.java
+++ src/main/java/de/prob2/jupyter/commands/HelpCommand.java
@@ -1,7 +1,12 @@
 package de.prob2.jupyter.commands;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.TreeMap;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import com.google.inject.Inject;
@@ -17,6 +22,41 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public final class HelpCommand implements Command {
+	private static final @NotNull Map<@NotNull String, @NotNull List<@NotNull Class<? extends Command>>> COMMAND_CLASS_CATEGORIES;
+	static {
+		final Map<String, List<Class<? extends Command>>> commandCategories = new LinkedHashMap<>();
+		commandCategories.put("Evaluation", Collections.unmodifiableList(Arrays.asList(
+			EvalCommand.class,
+			SolveCommand.class,
+			TableCommand.class,
+			TypeCommand.class,
+			PrettyPrintCommand.class,
+			LetCommand.class,
+			UnletCommand.class,
+			AssertCommand.class
+		)));
+		commandCategories.put("Animation", Collections.unmodifiableList(Arrays.asList(
+			LoadCellCommand.class,
+			LoadFileCommand.class,
+			ConstantsCommand.class,
+			InitialiseCommand.class,
+			ExecCommand.class,
+			BrowseCommand.class,
+			TraceCommand.class,
+			GotoCommand.class,
+			FindCommand.class
+		)));
+		commandCategories.put("Visualisation", Collections.unmodifiableList(Arrays.asList(
+			ShowCommand.class,
+			DotCommand.class
+		)));
+		commandCategories.put("Verification", Collections.unmodifiableList(Arrays.asList(
+			CheckCommand.class,
+			ModelCheckCommand.class
+		)));
+		COMMAND_CLASS_CATEGORIES = Collections.unmodifiableMap(commandCategories);
+	}
+	
 	private final @NotNull Injector injector;
 	
 	@Inject
@@ -52,18 +92,50 @@ public final class HelpCommand implements Command {
 		final List<String> args = CommandUtils.splitArgs(argString);
 		if (args.isEmpty()) {
 			final StringBuilder sb = new StringBuilder("Enter a B expression or predicate to evaluate it.\nYou can also use any of the following commands. For more help on a particular command, run :help commandname.\n");
-			final StringBuilder sbMarkdown = new StringBuilder("Enter a B expression or predicate to evaluate it.\n\nYou can also use any of the following commands. For more help on a particular command, run `:help commandname`.\n\n");
-			new TreeMap<>(kernel.getCommands()).forEach((commandName, command) -> {
-				sb.append(commandName);
-				sb.append(" - ");
-				sb.append(command.getShortHelp());
-				sb.append('\n');
-				sbMarkdown.append("* `");
-				sbMarkdown.append(commandName);
-				sbMarkdown.append("` - ");
-				sbMarkdown.append(command.getShortHelp());
-				sbMarkdown.append('\n');
+			final StringBuilder sbMarkdown = new StringBuilder("Enter a B expression or predicate to evaluate it.\n\nYou can also use any of the following commands. For more help on a particular command, run `:help commandname`.\n");
+			
+			final Map<Class<? extends Command>, Command> commandsByClass = kernel.getCommands()
+				.values()
+				.stream()
+				.collect(Collectors.toMap(
+					command -> command.getClass().asSubclass(Command.class),
+					command -> command
+				));
+			final Map<String, List<Command>> commandCategories = new LinkedHashMap<>();
+			COMMAND_CLASS_CATEGORIES.forEach((categoryName, commandClasses) ->
+				commandCategories.put(
+					categoryName,
+					commandClasses.stream()
+						.map(commandsByClass::get)
+						.collect(Collectors.toList())
+				)
+			);
+			final List<Command> uncategorizedCommands = new ArrayList<>(kernel.getCommands().values());
+			commandCategories.values().forEach(uncategorizedCommands::removeAll);
+			uncategorizedCommands.sort(Comparator.comparing(Command::getName));
+			commandCategories.put("Other", uncategorizedCommands);
+			
+			commandCategories.forEach((categoryName, commands) -> {
+				sb.append("\n");
+				sb.append(categoryName);
+				sb.append(":\n");
+				sbMarkdown.append("\n## ");
+				sbMarkdown.append(categoryName);
+				sbMarkdown.append("\n\n");
+				
+				commands.forEach(command -> {
+					sb.append(command.getName());
+					sb.append(" - ");
+					sb.append(command.getShortHelp());
+					sb.append('\n');
+					sbMarkdown.append("* `");
+					sbMarkdown.append(command.getName());
+					sbMarkdown.append("` - ");
+					sbMarkdown.append(command.getShortHelp());
+					sbMarkdown.append('\n');
+				});
 			});
+			
 			final DisplayData result = new DisplayData(sb.toString());
 			result.putMarkdown(sbMarkdown.toString());
 			return result;
